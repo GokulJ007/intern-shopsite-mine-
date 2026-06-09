eb1a5bf396c29f8275c08671c2396ce25540a9df
Gokul <togokulj@gmail.com>
2026-06-09 13:19:34 +0530

    refactor: use env variables for database connection

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,20 @@
+import os
 import pymysql
 import pymysql.cursors
+from dotenv import load_dotenv
+
+# Load env variables (in case database.py is imported directly elsewhere)
+load_dotenv()
 
 def DB_connection():
     try:
         connector = pymysql.connect(
-            host='localhost',
-            user='root',
-            password='Gokulj7959$',
-            database='fakedb',
-            port=3030,
+            host=os.getenv('DB_HOST', 'localhost'),
+            user=os.getenv('DB_USER', 'root'),
+            password=os.getenv('DB_PASSWORD', ''),
+            database=os.getenv('DB_NAME', 'defaultdb'),
+            port=int(os.getenv('DB_PORT', 3306)),
+            ssl={'ssl_mode': 'REQUIRED'},
             cursorclass=pymysql.cursors.DictCursor,
         )
         return connector
